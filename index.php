2d8e8a24909f99903ca82aabb18e95a4e25bf7a3
webkubator17 <webkubator@gmail.com>
2026-09-08 19:53:05 +0700

    content: update launched websites count to 52 plus

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ header('Pragma: no-cache');
                     </div>
                     <div class="trust-row" aria-label="Pencapaian Webkubator">
                         <div><strong>5+</strong><span>Tahun pengalaman</span></div>
-                        <div><strong>52</strong><span>Website diluncurkan</span></div>
+                        <div><strong>52+</strong><span>Website diluncurkan</span></div>
                         <div><strong>25+</strong><span>Klien bertumbuh</span></div>
                     </div>
                 </div>
